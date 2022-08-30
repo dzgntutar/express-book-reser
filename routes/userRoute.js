a9a844abbfdd1dc7b6a2dbe998c9d2d86b737918
dzgntutar <duzguntutar@gmail.com>
2022-08-30 18:34:22 +0300

    login form

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -11,13 +11,27 @@ userRouter.get("/", async (req, res) => {
 userRouter.post("/", async (req, res) => {
   try {
     User.create(req.body);
+    res.render("/user");
   } catch (error) {
-    console.log(req.body);
+    res.status(404).render("error");
   }
 });
 
 userRouter.get("/login", (req, res) => {
   res.status(200).render("login");
+});
+
+userRouter.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
+
+  let user = User.findOne(email, password);
+  if (user) {
+    res.status(200).redirect("/dashboard");
+  } else {
+    res.status(404).redirect("/login");
+  }
 });
 
 export { userRouter };
